4728ba6076e4114b37f623b88333f10b581c976a
Vouie <107463184+OfficialVouie@users.noreply.github.com>
2026-03-09 19:45:24 -0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { getFirestore, collection, addDoc, getDocs, query, orderBy } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyClmE36o3vkujTdpSS84gBf-0DQIVlWrPQ",
+  authDomain: "loom-ef2c8.firebaseapp.com",
+  projectId: "loom-ef2c8",
+  storageBucket: "loom-ef2c8.firebasestorage.app",
+  messagingSenderId: "774949013487",
+  appId: "1:774949013487:web:cf464b6b9db7d93cd7981a",
+  measurementId: "G-SKHVX0J065"
+};
+
+// Initialize
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const storage = getStorage(app);
+const db = getFirestore(app);
+
+export { storage, db, ref, uploadBytesResumable, getDownloadURL, collection, addDoc, getDocs, query, orderBy };
